544787461eca4e0d6aa7d52dbe2981b4184a7c54
syabdull <syab5050@gmail.com>
2024-09-09 11:47:45 +0530

    condtions added

---
+++ BasicDSA/condition.js
@@ -0,0 +1,13 @@
+//working with if  and else and working with nested  if and else
+
+//check the given number is odd  or even
+
+function isEven(n) {
+  if (n % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEven(10));
